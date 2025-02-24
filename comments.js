7c585c3db54751caed3659ac0b9f01f3786aca3d
qiexia <54071264+qiexia@users.noreply.github.com>
2025-02-24 19:36:11 +0000

    this thing wasn't taking

--- comments.js
+++ comments.js
@@ -1,60 +1,21 @@
-
 //create web server
-var http = require("http");
-var express = require("express");
-var app = express();
-var server = http.createServer(app);
-var io = require("socket.io")(server);
-var fs = require("fs");
 
-//create web server
-server.listen(3000, function() {
-    console.log("Server listening at port 3000");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a router
+const router = express.Router();
+
+// Define a simple route
+router.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-//send HTML file to client
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
+// Use the router
+app.use('/', router);
 
-//send CSS file to client
-app.get("/style.css", function(req, res) {
-    res.sendFile(__dirname + "/style.css");
-});
-
-//send JS file to client
-app.get("/client.js", function(req, res) {
-    res.sendFile(__dirname + "/client.js");
-});
-
-//create array to store comments
-var comments = [];
-
-//send comments to client
-io.on("connection", function(socket) {
-    socket.emit("loadComments", comments);
-});
-
-//receive comment from client
-io.on("connection", function(socket) {
-    socket.on("newComment", function(comment) {
-        comments.push(comment);
-        io.emit("updateComments", comments);
-    });
-});
-
-//receive delete comment from client
-io.on("connection", function(socket) {
-    socket.on("deleteComment", function(index) {
-        comments.splice(index, 1);
-        io.emit("updateComments", comments);
-    });
-});
-
-//receive edit comment from client
-io.on("connection", function(socket) {
-    socket.on("editComment", function(data) {
-        comments[data.index] = data.comment;
-        io.emit("updateComments", comments);
-    });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
